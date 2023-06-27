9f98ad5dc74d5104c56d1fb406c26708d35ae37e
Developer <arcitec2301@gmail.com>
2023-06-27 03:38:21 -0600

    update typo

--- config-service/src/main/java/com/att/store/config/ConfigServiceApplication.java
+++ config-service/src/main/java/com/att/store/config/ConfigServiceApplication.java
@@ -2,8 +2,9 @@ package com.att.store.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
-@EnableConfigService
+@EnableConfigServer
 @SpringBootApplication
 public class ConfigServiceApplication {
 
