d4184ee15b17c8cf759673cc3dddc9e1a108b2e9
Developer <arcitec2301@gmail.com>
2023-06-30 02:32:13 -0600

    registry configuration completed

--- service-product/src/main/java/com/att/shopping/product/ServiceProductApplication.java
+++ service-product/src/main/java/com/att/shopping/product/ServiceProductApplication.java
@@ -3,6 +3,7 @@ package com.att.shopping.product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class ServiceProductApplication {
 
