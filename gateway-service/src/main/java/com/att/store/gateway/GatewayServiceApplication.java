ad1424954167f9ffe0bb549e763292eb753ed8e0
Developer <arcitec2301@gmail.com>
2023-07-08 23:15:22 -0600

    Gateway update

--- gateway-service/src/main/java/com/att/store/gateway/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/att/store/gateway/GatewayServiceApplication.java
@@ -2,7 +2,9 @@ package com.att.store.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class GatewayServiceApplication {
 
